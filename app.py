918ec652885c989805747e558bc630def23611f2
Diwakartalwar <Diwakartalwar44@gmail.com>
2026-04-09 16:47:43 +0530

    Update app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="My Streamlit App", page_icon=":unified mentor:", layout="centered")
+st.title("Hello, Streamlit!")
+st.write("This is a simple Streamlit app.")
